48f478bb607a6e0889cb399491a547fe42856282
Nikki Gowan <nikkigowan2@gmail.com>
2024-03-04 17:37:35 -0500

    Update emotion_detection.py, output formatting

--- emotion_detection.py
+++ emotion_detection.py
@@ -11,7 +11,24 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document" : { "text" : text_to_analyze } }
     header = { "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock" }
     
-    #send info over POST request and return response text
+    #send info over POST request and store response
     response = requests.post(url, json = myobj, headers = header, timeout = 10)
-    return response.text
-    
+    #formated response as json, dictionary of dictionaries
+    formatted_response = json.loads(response.text)
+    #return formatted_response['emotionPredictions'][0]['emotion']['anger']
+
+    #retrieve required set of emotions
+    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    #find the dominant emotion and store name
+    emotions = {'anger' : anger, 'disgust' : disgust, 'fear' : fear,
+    'joy' : joy, 'sadness' : sadness}
+    dominant_emotion = max(emotions, key = emotions.get)
+
+    #return the emotions, and the dominant emotion, from the response
+    return { 'anger' : anger, 'disgust' : disgust, 'fear' : fear,'joy' : joy, 
+    'sadness' : sadness, 'dominant_emotion' : dominant_emotion}
