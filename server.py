a0a7ffedc8329d26af1444a3ac50a6ce9293ce67
Nikki Gowan <nikkigowan2@gmail.com>
2024-03-05 11:03:56 -0500

    Update server.py, emotion detection route

--- server.py
+++ server.py
@@ -3,6 +3,29 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
+@app.route("/emotionDetector")
+def emo_detect():
+    """
+    function that receives text from the HTML interface and 
+    uses emotion_detector() to analyze what emotions are 
+    present, and what the dominant emotion of the text is
+    returns:
+    string that gives the calculated percentage of each emotion,
+    and the calculated dominant emotion
+    """
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    return f"""For the given statement, the system response is: 'anger':{anger}, 
+    'disgust':{disgust}, 'fear':{fear}, 'joy':{joy}, and 'sadness':{sadness}. 
+    The dominant emotion is <b>{dominant_emotion}</b>."""
+
 @app.route("/")
 def render_index_page():
     """
