2f9403a257a28795df3b59eb9707f91f3cff1575
Nikki Gowan <nikkigowan2@gmail.com>
2024-03-07 11:43:14 -0500

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -39,6 +39,13 @@ def emotion_detector(text_to_analyze):
         joy = None
         sadness = None
         dominant_emotion = None
+    else:
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+        dominant_emotion = None
 
     #return the emotions, and the dominant emotion, from the response
     return { 'anger' : anger, 'disgust' : disgust, 'fear' : fear,'joy' : joy, 
